a3c928315814795ecdbf394df5ceed29d3a4f78f
Rusty Davis <zopppo@gmail.com>
2015-05-21 10:27:01 -0600

    Remove with usage from runabft_random_matrices.py to support python2.6

--- runabft_random_matrices.py
+++ runabft_random_matrices.py
@@ -9,26 +9,31 @@ prob_table = "probability.txt"
 count = int(subprocess.check_output('cat ' + prob_table + ' | wc -l', shell=True)) - 1
 # The number of runs per trial
 
-with open(output_file, 'w') as out, open(prob_table, 'r') as prob:
-    oldProbability = ""
-    for i in range(0, count + 1):
-            currentProbability = prob.readline()
-            if currentProbability != oldProbability:
-                out.write(currentProbability.rstrip("\n") + "%" + '\n')
-                oldProbability = currentProbability
+out = open(output_file, 'w')
+prob = open(prob_table, 'r')
 
-            # Randomly create matrices
-            subprocess.call(['./makeMatrix', '-o', 'A.dat', '-m', '45', '-n', '45', '-l', '0', '-u', '127'])
-            subprocess.call(['./makeMatrix', '-o', 'B.dat', '-m', '45', '-n', '45', '-l', '0', '-u', '127'])
-            # Check sum the matrices
-            subprocess.call(['./checksumA', '-a', 'A.dat', '-o' , 'Aprime.dat'])
-            subprocess.call(['./checksumB', '-b', 'B.dat', '-o' , 'Bprime.dat'])
-            # Multiply the matrices to create the golden Cprime
-            subprocess.call(['./multiplyMatrix', '-a', 'Aprime.dat', '-b', 'Bprime.dat', '-o', 'Cprime.dat'])
-            # Run the abft driver with the matrices
-            output = subprocess.check_output("./run.sh", shell=True)
-            # decode the output from a binary string to utf-8
-            output = output.decode("utf-8")
-            out.write(output)
+oldProbability = ""
+for i in range(0, count + 1):
+    currentProbability = prob.readline()
+    if currentProbability != oldProbability:
+        out.write(currentProbability.rstrip("\n") + "%" + '\n')
+        oldProbability = currentProbability
+
+    # Randomly create matrices
+    subprocess.call(['./makeMatrix', '-o', 'A.dat', '-m', '45', '-n', '45', '-l', '0', '-u', '127'])
+    subprocess.call(['./makeMatrix', '-o', 'B.dat', '-m', '45', '-n', '45', '-l', '0', '-u', '127'])
+    # Check sum the matrices
+    subprocess.call(['./checksumA', '-a', 'A.dat', '-o' , 'Aprime.dat'])
+    subprocess.call(['./checksumB', '-b', 'B.dat', '-o' , 'Bprime.dat'])
+    # Multiply the matrices to create the golden Cprime
+    subprocess.call(['./multiplyMatrix', '-a', 'Aprime.dat', '-b', 'Bprime.dat', '-o', 'Cprime.dat'])
+    # Run the abft driver with the matrices
+    output = subprocess.check_output("./run.sh", shell=True)
+    # decode the output from a binary string to utf-8
+    output = output.decode("utf-8")
+    out.write(output)
+
+out.close()
+prob.close()
 #os.sys("./run.sh >> $OUTPUTFILE")
 
